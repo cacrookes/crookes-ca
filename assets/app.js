013780c4fb237f62e04598b6357c9c1dd79bfa91
Christopher Crookes <cacrookes@gmail.com>
2015-02-19 15:52:12 -0800

    Creating education page. Still need to fix image and file locations

--- assets/app.js
+++ assets/app.js
@@ -36,8 +36,48 @@ app.controller('AboutCtrl', function(){
 
 });
 
-app.controller('EducationCtrl', function(){
+app.controller('EducationCtrl', function($scope){
+	//defines completed courses in continuing education section
+	$scope.courses = [
+		{
+			name: 'BerkeleyX: CS188.1x Artificial Intelligence',
+			course_link: 'https://www.edx.org/course/uc-berkeleyx/uc-berkeleyx-cs188-1x-artificial-579#.U0HdDPldXng',
+			certificate_link: '//s3.amazonaws.com/verify.edx.org/downloads/06060b8569da4de5a7ab58b13859188c/Certificate.pdf',
+			school: 'edx'
+		},
+		{
+			name: 'Algorithms: Design and Analysis, Part 1',
+			course_link: 'https://www.coursera.org/course/algo',
+			certificate_link: 'files/algo-certificate.pdf',
+			school: 'coursera'
+		},
+		{
+			name: 'Genes and the Human Condition (From Behavior to Biotechnology)',
+			course_link: 'https://www.coursera.org/course/genes',
+			certificate_link: 'files/Coursera-genes.pdf',
+			school: 'coursera'
+		},
+		{
+			name: 'HarvardX: PH201x: Health and Society',
+			course_link: 'https://www.edx.org/course/harvardx/harvardx-ph201x-health-society-905#.U0HeGvldXng',
+			certificate_link: 'https://s3.amazonaws.com/verify.edx.org/downloads/9d19bd661f5247e9816278159c972598/Certificate.pdf',
+			school: 'edx'
+		}
 
+	];
+	
+	//returns school logo
+	$scope.getLogo = function(school){
+		console.log('getting logo for ' + school);
+		switch(school){
+			case 'edx':
+				return '/files/edx.png';
+				break;
+			case 'coursera':
+				return '/files/coursera.png';
+				break;
+		}
+	};
 });
 
 app.controller('SkillsCtrl', function(){
@@ -85,7 +125,7 @@ app.service('MailSvc', function ($http){
 app.directive('navPanel', function(){
 	return {
 		restrict: 'E',
-		templateUrl: 'nav-panel.html',
+		templateUrl: '/templates/nav-panel.html',
 		controller: function($scope, $location){
 			//stores site links
 			$scope.navlinks = [
