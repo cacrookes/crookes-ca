b1d42fc8759b0bc71a49a9420ce64b312b0224b7
Christopher Crookes <cacrookes@gmail.com>
2015-02-26 15:04:20 -0800

    Updating education page. Removing getLogo function since it is called on every digest and code will still be easy to maintain by hardcoding logo locations

--- ng/education.ctrl.js
+++ ng/education.ctrl.js
@@ -6,39 +6,31 @@ angular.module('app')
 			name: 'BerkeleyX: CS188.1x Artificial Intelligence',
 			course_link: 'https://www.edx.org/course/uc-berkeleyx/uc-berkeleyx-cs188-1x-artificial-579#.U0HdDPldXng',
 			certificate_link: '//s3.amazonaws.com/verify.edx.org/downloads/06060b8569da4de5a7ab58b13859188c/Certificate.pdf',
-			school: 'edx'
+			school: 'edx',
+      school_logo: '/img/edx.png'
 		},
 		{
 			name: 'Algorithms: Design and Analysis, Part 1',
 			course_link: 'https://www.coursera.org/course/algo',
 			certificate_link: 'files/algo-certificate.pdf',
-			school: 'coursera'
+			school: 'coursera',
+      school_logo: '/img/coursera.png'
 		},
 		{
 			name: 'Genes and the Human Condition (From Behavior to Biotechnology)',
 			course_link: 'https://www.coursera.org/course/genes',
 			certificate_link: 'files/Coursera-genes.pdf',
-			school: 'coursera'
+			school: 'coursera',
+      school_logo: '/img/coursera.png'
 		},
 		{
 			name: 'HarvardX: PH201x: Health and Society',
 			course_link: 'https://www.edx.org/course/harvardx/harvardx-ph201x-health-society-905#.U0HeGvldXng',
 			certificate_link: 'https://s3.amazonaws.com/verify.edx.org/downloads/9d19bd661f5247e9816278159c972598/Certificate.pdf',
-			school: 'edx'
+			school: 'edx',
+      school_logo: '/img/edx.png'
 		}
 
 	];
 
-	//returns school logo
-	$scope.getLogo = function(school){
-		console.log('getting logo for ' + school);
-		switch(school){
-			case 'edx':
-				return '/files/edx.png';
-				break;
-			case 'coursera':
-				return '/files/coursera.png';
-				break;
-		}
-	};
 });
