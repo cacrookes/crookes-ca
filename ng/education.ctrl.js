dbc0630697f271dcb9e7873a2ad15b98ae5491c0
Christopher Crookes <cacrookes@gmail.com>
2015-03-14 10:48:42 -0700

    Changing appearance of Education page

--- ng/education.ctrl.js
+++ ng/education.ctrl.js
@@ -7,31 +7,32 @@ angular.module('app')
 			name: 'BerkeleyX: CS188.1x Artificial Intelligence',
 			course_link: 'https://www.edx.org/course/uc-berkeleyx/uc-berkeleyx-cs188-1x-artificial-579#.U0HdDPldXng',
 			certificate_link: '//s3.amazonaws.com/verify.edx.org/downloads/06060b8569da4de5a7ab58b13859188c/Certificate.pdf',
-			school: 'edx',
-      school_logo: '/img/edx.png'
+			school: 'edx'
 		},
 		{
 			name: 'Algorithms: Design and Analysis, Part 1',
 			course_link: 'https://www.coursera.org/course/algo',
 			certificate_link: 'files/algo-certificate.pdf',
-			school: 'coursera',
-      school_logo: '/img/coursera.png'
+			school: 'coursera'
 		},
 		{
 			name: 'Genes and the Human Condition (From Behavior to Biotechnology)',
 			course_link: 'https://www.coursera.org/course/genes',
 			certificate_link: 'files/Coursera-genes.pdf',
-			school: 'coursera',
-      school_logo: '/img/coursera.png'
+			school: 'coursera'
 		},
 		{
 			name: 'HarvardX: PH201x: Health and Society',
 			course_link: 'https://www.edx.org/course/harvardx/harvardx-ph201x-health-society-905#.U0HeGvldXng',
 			certificate_link: 'https://s3.amazonaws.com/verify.edx.org/downloads/9d19bd661f5247e9816278159c972598/Certificate.pdf',
-			school: 'edx',
-      school_logo: '/img/edx.png'
+			school: 'edx'
+		},
+		{
+			name: 'HTML5 Game Development',
+			course_link: 'https://www.udacity.com/course/cs255',
+			certificate_link: 'files/html5-game-dev-certificate.pdf',
+			school: 'udacity'
 		}
-
 	];
 
 });
