dae83cfe437222263f2cd563a7a3cf12d711c0b3
Christopher Crookes <cacrookes@gmail.com>
2015-07-19 16:04:26 -0700

    Added form validation and some styles for contact form

--- ng/contact.ctrl.js
+++ ng/contact.ctrl.js
@@ -1,32 +1,67 @@
+/* This controller handles the contact form. If all of the fields pass validation,
+the form data is sent to the mail service (mail.svc.js) which posts the form data
+to the server */
+
 angular.module('app')
-.controller('ContactCtrl', function($scope, MailSvc){
-	$scope.pageId = 'contactPage'
-	$scope.result = 'hidden';
+.controller('ContactCtrl', function($scope, MailSvc, $timeout){
+	$scope.pageId = 'contactPage';
+
+	//define model for contact form
+	$scope.emailData = {
+		inputName: "",
+		inputEmail: "",
+		inputMessage: ""
+	};
+	//stores original form values. Handy for resetting the form.
+	var oriEmailData = angular.copy($scope.emailData);
+
+	// resets the form. Resets modal data, and sets the angular attributes: 'pristine'
+	// and 'untouched' to the form inputs.
+	var resetForm = function(){
+		$scope.contactform.$setPristine();
+		$scope.contactform.$setUntouched();
+		$scope.emailData = angular.copy(oriEmailData);
+	};
+
+
+	//resultMessage displays a success or failure message below the form
 	$scope.resultMessage = "";
-	$scope.formData;
-	$scope.submitButtonDisabled = false;
-	$scope.submitted = false;
+	$scope.showResult = false;
 	$scope.submit = function(contactform){
-		$scope.submitted = true;
-		$scope.submitButtonDisabled = true;
+		//checks if form passes clientside validtation
 		if (contactform.$valid) {
-			console.log("posting mail");
-			console.log($scope.formData);
-			MailSvc.send($scope.formData)
+			resetForm();
+			//display confirmation message
+			$scope.resultMessage = "Message sent!";
+			$scope.showResult = true;
+			//hide message after 10 seconds
+			$timeout(function(){
+					$scope.showResult = false;
+			}, 5000);
+
+/*			MailSvc.send($scope.emailData)
 			.success(function(data){
-				console.log("Return data: " + data);
-				$scope.submitButtonDisable = true;
+				//mail service returned the mail was successfully sent
 				$scope.resultMessage = "Message sent!";
-				$scope.result = 'bg-success';
+				alert("Message Sent! Thank you!");
+				//clear the form
+				$scope.contactform.$setPristine();
+				$scope.contactform.$setUntouched();
+				$scope.contactform.emailData={
+					inputName: "",
+					inputEmail: "",
+					inputMessage: ""
+				};
 			}).error(function(data, status){
-				$scope.submitButtonDisabled = false;
+				//mail service returned an error
 				$scope.resultMessage = data.message;
-				$scope.result='bg-danger';
-			});
+				alert("Error in sending message: " + data.message);
+			});*/
 		}else{
-			$scope.submitButtonDisabled = false;
-			$scope.resultMessage = 'Failed: Please fill out all fields';
-			$scope.result='bg-danger';
+			//form did not pass clientside validation. Since the submit button
+			//is not active if the form is invalid, this should never be reached.
+			//$scope.resultMessage = 'Failed: Please fill out all fields';
+			//alert("Please fill out all fields before sending");
 		}
-	}
+	};
 });
