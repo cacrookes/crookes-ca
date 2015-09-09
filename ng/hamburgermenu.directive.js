1c706d1b83b7aea712c12656c04ecd9081c21a64
Christopher Crookes <cacrookes@gmail.com>
2015-09-09 00:06:45 -0700

    Implemented hamburger menu

---
+++ ng/hamburgermenu.directive.js
@@ -0,0 +1,25 @@
+angular.module('app')
+.directive('hamburgerPanel', function(){
+	return {
+		restrict: 'E',
+		templateUrl: '/templates/hamburger-panel.html',
+		controller: function($scope, $location){
+			//stores site links
+			$scope.navlinks = [
+				{title: 'About', icon: 'face', path: '/about'},
+				{title: 'Education', icon: 'school', path: '/education'},
+				{title: 'Skills', icon: 'build', path: '/skills'},
+				{title: 'Contact', icon: 'email', path: '/contact'}
+			];
+
+			//check if link is for the active page, and if so returns true
+			$scope.isActive = function(navlink){
+				if (navlink.path == $location.path()){
+					return true;
+				}
+				return false;
+			};
+		},
+		controllerAs: 'hamburgerpanel'
+	};
+});
